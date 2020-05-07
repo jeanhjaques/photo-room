89dac9786945eb5acb7485c0d653b988df1b9798
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2020-05-07 14:45:41 -0400

    Update perfil.php

--- sources/app/views/perfil/perfil.php
+++ sources/app/views/perfil/perfil.php
@@ -1,26 +1,39 @@
-            <!-- Bloco onde fica o albúm do usuário -->
-            <div class="imagens">
-                <nav class="caixadeimagens">
-                    <ul>
-                        <li><img onclick="abrirImagem('../../public/foto0.jpg', 'foto0');" id="a" class="imgcx img" src="../../../public/img/foto0.jpg" alt="foto0"></li>
-                        <li><img onclick="abrirImagem('../../public/foto1.jpg' , 'foto1');" class="imgcx img" src="../../../public/img/foto1.jpg" alt="foto1"></li>
-                        <li><img onclick="abrirImagem('../../public/foto2.jpg', 'foto2');" class="imgcx img" src="../../../public/img/foto2.jpg" alt="foto2"></li>
-                        <li><img onclick="abrirImagem('../../public/foto3.jpg', 'foto3');" class="imgcx img" src="../../../public/img/foto3.jpg" alt="foto3"></li>
-                        <li><img onclick="abrirImagem('../../public/foto4.jpg', 'foto4');" class="imgcx img" src="../../../public/img/foto4.jpg" alt="foto4"></li>
-                        <li><img onclick="abrirImagem('../../public/foto5.jpg', 'foto5');" class="imgcx img" src="../../../public/img/foto5.jpg" alt="foto5"></li>
-                        <li><img onclick="abrirImagem('../../public/foto6.jpg', 'foto6');" class="imgcx img" src="../../../public/img/foto6.jpg" alt="foto6"></li>
-                        <li><img onclick="abrirImagem('../../public/foto7.jpg', 'foto 7');" class="imgcx img" src="../../../public/img/foto7.jpg" alt="foto7"></li>
-                     
-                    </ul>
-                </nav>
-            </div>
-        </article>
-
-        <footer>
-            <!-- Espaço com informação dos DEVs -->
-            <p>Desenvolvido por Fenix Group</p>
-            <p>Copyright</p>
-            <p>2020</p>
-        </footer>
-
-    </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="perfil.css">
+</head>
+<body>
+    <article>
+        <h1>Informações do perfil</h1>
+        <hr>
+        <!-- Imagem de perfil do usuário -->
+        <figure>
+            <img class="imagemPerfil" src="../../../public/img/imgperfil.jpg" alt="imgperfil">
+        </figure>
+        <!-- Informações do perfil do usuário -->
+        <p>
+            Nome: Suellen Rosemberg dos Santos
+        </p>
+        <p>
+            Email: suellenrosemberg10@gmail.com
+        </p>
+        <p>
+            País: Brasil
+        </p>
+        <p>
+            Código da sua conta: 000001
+        </p>
+        <p>
+            Cidade: Campo Grande
+        </p>
+        <p>
+            Telefone: (99) 99999 - 9999
+        </p>
+    </article>
+</body>
+</html>
+© 2020 GitHub, Inc.
