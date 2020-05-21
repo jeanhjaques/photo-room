4faf7814ccf89b4a3c5817077e4f047aaccec115
Jean Jaques <jean.jaques@aluno.ufms.br>
2020-05-20 22:11:49 -0400

    Add classe Conexao.php com o objetivo de controlar o BD

---
+++ sources/app/models/Conexao.php
@@ -0,0 +1,20 @@
+<?php
+//classe responsável pela configuração da conexão com o banco de dados
+class Conexao{
+    private static $instance;
+
+    public static function getConnect(){
+        if(!isset(self::$instance)){
+            //Conexão BD
+            self::$instance = new \PDO('psql:host=localhost;dbname=photoroom;', 'postgres', '201619060485');
+            //Seleciona o esquema do BD
+            $resultadoConexaoSchema = self::$instance->exec("SET search_path TO photoroomschema");
+            //
+            if(!resultado){
+                //Caso conexão com o schema seja falha o erro é reportado
+                die('Failed to set schema: '.self::$instance->errorInfo());
+            }
+        }
+        return self::$instance;
+    }
+}
