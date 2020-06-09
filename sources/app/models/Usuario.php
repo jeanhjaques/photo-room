46900731fad655d8c86cc496bfa560c917a1a628
Jean Jaques <jean.jaques@aluno.ufms.br>
2020-06-08 21:51:10 -0400

    correcao hack

--- sources/app/models/Usuario.php
+++ sources/app/models/Usuario.php
@@ -4,304 +4,153 @@
      */
 error_reporting(E_ALL);
      class Usuario{
-        /**
-         * @var int Id do usuário
-         */
+
         private $id;
-
-        /**
-         * @var string Nome do usuário 
-         */
-         private $nome;
-
-         /**
-          * @var string Sobrenome do usuário
-          */
-         private $sobrenome;
-
-          /**
-         * @var string Data de nascimento do usuário
-         */
+        private $nome;
+        private $sobrenome;
         private $dataNascimento;
-
-        /**
-         * @var string Email do usuário
-         */
         private $email;
-
-        /**
-         * @var string Senha do usuário
-         */
         private $senha;
-
-        /**
-         * @var string Cidade do usuário
-         */
         private $cidade;
-        
-        /**
-         * @var string Estado do usuário
-         */
         private $estado;
-
-        /**
-         * @var string País do usuário
-         */
         private $pais;
-
-        /**
-         * @var string Telefone do usuário
-         */
         private $telefone;
-
-         /**
-         * @var string Foto do perfil do usuário
-         */
         private $foto_perfil;
-
-         /**
-         * @var Album Armazena um album principal do usuário
-         */
         private $albumPrincipal;
-
-         /**
-         * @var Album Armazena um album favorito do usuário
-         */
         private $albumFavorito;
-                
-        /**
-         * Construtor da classe que depende do nome, sobrenome, email, senha, cidade,
-         * estado, pais, telefone e foto do perfil.
-         */
-        
-        function __construct(string $nome, string $sobrenome, string $dataNascimento, string $email, string $senha,
-                                string $cidade, string $estado, string $pais){
-
-            $this->nome = $nome;
-            $this->sobrenome = $sobrenome;
-            $this->dataNascimento = $dataNascimento;
-            $this->email = $email;
-            $this->senha = $senha;
-            $this->cidade = $cidade;
-            $this->estado = $estado;
-            $this->pais = $pais;
-        }        
 
          /**
-          * @return int
+          * Usuario constructor.
+          * @param $nome
+          * @param $sobrenome
+          * @param $dataNascimento
+          * @param $email
+          * @param $senha
+          * @param $cidade
+          * @param $estado
+          * @param $pais
           */
-         public function getId(): int
-         {
-             return $this->id;
-         }
 
-         /**
-          * @param int $id
-          */
-         public function setId(int $id): void
-         {
-             $this->id = $id;
-         }
-
-         /**
-          * @return string
-          */
-         public function getNome(): string
-         {
-             return $this->nome;
-         }
-
-         /**
-          * @param string $nome
-          */
-         public function setNome(string $nome): void
+         public function __construct($nome, $sobrenome, $dataNascimento, $email, $senha, $cidade, $estado, $pais)
          {
              $this->nome = $nome;
-         }
-
-         /**
-          * @return string
-          */
-         public function getSobrenome(): string
-         {
-             return $this->sobrenome;
-         }
-
-         /**
-          * @param string $sobrenome
-          */
-         public function setSobrenome(string $sobrenome): void
-         {
              $this->sobrenome = $sobrenome;
-         }
-
-         /**
-          * @return string
-          */
-         public function getDataNascimento(): string
-         {
-             return $this->dataNascimento;
-         }
-
-         /**
-          * @param string $dataNascimento
-          */
-         public function setDataNascimento(string $dataNascimento): void
-         {
              $this->dataNascimento = $dataNascimento;
-         }
-
-         /**
-          * @return string
-          */
-         public function getEmail(): string
-         {
-             return $this->email;
-         }
-
-         /**
-          * @param string $email
-          */
-         public function setEmail(string $email): void
-         {
              $this->email = $email;
-         }
-
-         /**
-          * @return string
-          */
-         public function getSenha(): string
-         {
-             return $this->senha;
-         }
-
-         /**
-          * @param string $senha
-          */
-         public function setSenha(string $senha): void
-         {
              $this->senha = $senha;
-         }
-
-         /**
-          * @return string
-          */
-         public function getCidade(): string
-         {
-             return $this->cidade;
-         }
-
-         /**
-          * @param string $cidade
-          */
-         public function setCidade(string $cidade): void
-         {
              $this->cidade = $cidade;
-         }
-
-         /**
-          * @return string
-          */
-         public function getEstado(): string
-         {
-             return $this->estado;
-         }
-
-         /**
-          * @param string $estado
-          */
-         public function setEstado(string $estado): void
-         {
              $this->estado = $estado;
-         }
-
-         /**
-          * @return string
-          */
-         public function getPais(): string
-         {
-             return $this->pais;
-         }
-
-         /**
-          * @param string $pais
-          */
-         public function setPais(string $pais): void
-         {
              $this->pais = $pais;
          }
 
          /**
-          * @return string
-          */
-         public function getTelefone(): string
-         {
+         * Construtor da classe que depende do nome, sobrenome, email, senha, cidade,
+         * estado, pais, telefone e foto do perfil.
+         */
+
+
+
+         public function getId(){
+             return $this->id;
+         }
+
+         public function setId($id){
+             $this->id = $id;
+         }
+
+         public function getNome(){
+             return $this->nome;
+         }
+
+         public function setNome($nome){
+             $this->nome = $nome;
+         }
+
+         public function getSobrenome(){
+             return $this->sobrenome;
+         }
+
+         public function setSobrenome($sobrenome){
+             $this->sobrenome = $sobrenome;
+         }
+
+         public function getDataNascimento(){
+             return $this->dataNascimento;
+         }
+
+         public function setDataNascimento($dataNascimento){
+             $this->dataNascimento = $dataNascimento;
+         }
+
+         public function getEmail(){
+             return $this->email;
+         }
+
+         public function setEmail($email){
+             $this->email = $email;
+         }
+
+         public function getSenha(){
+             return $this->senha;
+         }
+
+         public function setSenha($senha){
+             $this->senha = $senha;
+         }
+
+         public function getCidade(){
+             return $this->cidade;
+         }
+
+         public function setCidade($cidade){
+             $this->cidade = $cidade;
+         }
+
+         public function getEstado(){
+             return $this->estado;
+         }
+
+         public function setEstado($estado){
+             $this->estado = $estado;
+         }
+
+         public function getPais(){
+             return $this->pais;
+         }
+
+         public function setPais($pais){
+             $this->pais = $pais;
+         }
+
+         public function getTelefone(){
              return $this->telefone;
          }
 
-         /**
-          * @param string $telefone
-          */
-         public function setTelefone(string $telefone): void
-         {
+         public function setTelefone($telefone){
              $this->telefone = $telefone;
          }
 
-         /**
-          * @return string
-          */
-         public function getFotoPerfil(): string
-         {
+         public function getFotoPerfil(){
              return $this->foto_perfil;
          }
 
-         /**
-          * @param string $foto_perfil
-          */
-         public function setFotoPerfil(string $foto_perfil): void
-         {
+         public function setFotoPerfil($foto_perfil){
              $this->foto_perfil = $foto_perfil;
          }
 
-         /**
-          * @return Album
-          */
-         public function getAlbumPrincipal(): Album
-         {
+         public function getAlbumPrincipal(){
              return $this->albumPrincipal;
          }
 
-         /**
-          * @param Album $albumPrincipal
-          */
-         public function setAlbumPrincipal(Album $albumPrincipal): void
-         {
+         public function setAlbumPrincipal($albumPrincipal){
              $this->albumPrincipal = $albumPrincipal;
          }
 
-         /**
-          * @return Album
-          */
-         public function getAlbumFavorito(): Album
-         {
+         public function getAlbumFavorito(){
              return $this->albumFavorito;
          }
 
-         /**
-          * @param Album $albumFavorito
-          */
-         public function setAlbumFavorito(Album $albumFavorito): void
-         {
+         public function setAlbumFavorito($albumFavorito){
              $this->albumFavorito = $albumFavorito;
          }
-
-         /**
-        *   Método que verifica se o email e senha providos são iguais ao da instância.
-        *   Sua importância é devido ao fato da senha ser codificada.
-        *
-        *   @return bool Retorna TRUE se igual, senão FALSE
-        */
-        public function igual(string $email, string $senha) {
-            return $this->email === $email && $this->senha === hash('sha256', $senha);
-        }
      }
