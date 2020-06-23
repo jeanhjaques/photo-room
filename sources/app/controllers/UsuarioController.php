496f3c0a545ae5c06252c7b21994704b457019f9
Suellen Rosemberg dos Santos <suellenrosemberg10@gmail.com>
2020-06-23 08:43:51 -0400

    Corrigindo mudança de imagem do perfil

--- sources/app/controllers/UsuarioController.php
+++ sources/app/controllers/UsuarioController.php
@@ -10,15 +10,22 @@
         public function atualizarPerfil($imagem, $nome, $sobrenome, $email, $pais, $cidade, $telefone)
         {
             $id = $_SESSION['usuarioLogado']['idusuario'];
-
-            $extensao = strtolower(substr($imagem['name'], -3));
-            $data = date('d-m-Y-H-i-s');
-            $novo_nome = $data . "." . $extensao;
-            $diretorio = "public/upload/";
-            move_uploaded_file($imagem['tmp_name'], $diretorio . $novo_nome);
-
+      
+            
+            if($imagem['name'] != null) {
+                $extensao = strtolower(substr($imagem['name'], -3));
+                $data = date('d-m-Y-H-i-s');
+                $novo_nome = $data . "." . $extensao;
+                $diretorio = "public/upload/";
+                move_uploaded_file($imagem['tmp_name'], $diretorio . $novo_nome);
+                $imagem = $novo_nome;
+            }else {
+                $imagem = $_SESSION['usuarioLogado']['endfotoperfil'];
+            }
+            
+                
             try {
-                usuarioDAO::atualizaPerfil($novo_nome, $nome, $sobrenome, $pais, $cidade, $telefone, $id);
+                usuarioDAO::atualizaPerfil($imagem, $nome, $sobrenome, $pais, $cidade, $telefone, $id);
                 $this->atualizarDadosUsuario($_SESSION['usuarioLogado']['email'],
                     $_SESSION['usuarioLogado']['senha']);
                 $this->perfil();
