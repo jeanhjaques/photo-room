9e6888ebdd0e971c0b062d2133d0dba68ad1cc0a
Jean Jaques <jean.jaques@aluno.ufms.br>
2020-06-26 01:34:37 -0400

    add title na pagina perfil

--- sources/app/views/perfil/perfil.php
+++ sources/app/views/perfil/perfil.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perfil - Photo Room</title>
     <link rel="stylesheet" href="app/views/perfil/perfil.css">
     <script src="app/views/perfil/SelecionaImg.js" type="text/javascript"></script> 
 </head>
